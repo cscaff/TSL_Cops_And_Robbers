b80b77719688edb70fca86824d04eb20cfa6335e
cscaff <cscaff@ufl.edu>
2025-10-27 12:52:10 -0400

    Added Robber Movement Functions/Classes to Entities.

--- test/game/entities.py
+++ test/game/entities.py
@@ -25,6 +25,23 @@ class Robber:
         self.x = x
         self.y = y
 
+    def moveL(x):
+        return x - 1
+    
+    def moveR(x):
+        return x + 1
+    
+    def moveU(y):
+        return y - 1
+    
+    def moveD(y):
+        return y + 1
+
+class _next_Robber:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 _MAX_M = None
 _MAX_N = None
 
